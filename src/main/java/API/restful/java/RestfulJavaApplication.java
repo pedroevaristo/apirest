9583e30b341e46fce38dcb392a97a5d319e9c039
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-11-14 18:44:01 -0300

    iniciando o projeto/desafio da dio

---
+++ src/main/java/API/restful/java/RestfulJavaApplication.java
@@ -0,0 +1,13 @@
+package API.restful.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/restful/java/RestfulJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package API.restful.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
